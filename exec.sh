b2035d43322aa8eda3d284dfdd6a466267a4dd5e
Vatsal Agarwal <144194722+vatsal-afk@users.noreply.github.com>
2024-06-20 23:12:10 +0530

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,17 +1,19 @@
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Assemble boot.asm
-nasm -f bin -o ~/Desktop/bootloader/boot.bin ~/Desktop/bootloader/boot.asm
+nasm -f bin -o "$script_dir/boot.bin" "$script_dir/boot.asm"
 
 # Assemble kernel.asm
-nasm -f bin -o ~/Desktop/bootloader/kernel.bin ~/Desktop/bootloader/kernel.asm
+nasm -f bin -o "$script_dir/kernel.bin" "$script_dir/kernel.asm"
 
 # Create a blank floppy image
-dd if=/dev/zero of=~/Desktop/bootloader/floppy.img bs=512 count=2880
+dd if=/dev/zero of="$script_dir/floppy.img" bs=512 count=2880
 
 # Write the bootloader to the image
-dd if=~/Desktop/bootloader/boot.bin of=~/Desktop/bootloader/floppy.img conv=notrunc
+dd if="$script_dir/boot.bin" of="$script_dir/floppy.img" conv=notrunc
 
 # Write the kernel to the image (starting at sector 2)
-dd if=~/Desktop/bootloader/kernel.bin of=~/Desktop/bootloader/floppy.img bs=512 seek=2 conv=notrunc
+dd if="$script_dir/kernel.bin" of="$script_dir/floppy.img" bs=512 seek=2 conv=notrunc
 
 # Test the floppy image using QEMU
-qemu-system-x86_64 -fda ~/Desktop/bootloader/floppy.img
+qemu-system-x86_64 -fda "$script_dir/floppy.img"
